2f2597d3da94a01a17525e415994a6f773b1a09b
m3llad0 <a01655451@tec.mx>
2022-11-17 13:52:52 -0600

    move model in Robot.py

--- ActIntegradora/Server/Robot.py
+++ ActIntegradora/Server/Robot.py
@@ -1,4 +1,6 @@
-from mesa import Agent
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
 
 class RobotAgent(Agent):
     """
@@ -80,7 +82,7 @@ class RobotAgent(Agent):
 
 class Box(Agent):
     """
-    Trash agent. Just to add dirty cells to the grid.
+    Box agent. 
     """
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
@@ -97,3 +99,55 @@ class dropZone(Agent):
 
     def step(self):
         pass
+class wallAgent(Agent):
+    """
+    Wall agent for grid delimitation.
+    """
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+    def step(self):
+        pass
+class RandomModel(Model):
+    """
+    Creates a new model with Roomba agents.
+    Args:
+        N: Number of agents in the simulation
+        height, width: The size of the grid to model
+    """
+    def __init__(self, N, BoxesN, width, height):
+        self.num_agents = N
+        self.num_boxes = BoxesN
+        self.remaning_boxes = BoxesN
+        self.grid = MultiGrid(width,height,torus = False)
+        self.schedule = RandomActivation(self)
+        self.running = True
+
+        # Places the boxes in the grid
+        for i in range(self.num_boxes):
+            boxy = Box(i+2000, self)
+            self.schedule.add(boxy)
+
+            pos_gen = lambda w, h: (self.random.randrange(w), self.random.randrange(h))
+            pos = pos_gen(self.grid.width, self.grid.height)
+            while (not self.grid.is_cell_empty(pos)):
+                pos = pos_gen(self.grid.width, self.grid.height)
+            self.grid.place_agent(boxy, pos)
+
+        # Add the agent to a random empty grid cell
+        for i in range(self.num_agents):
+            a = RobotAgent(i+1000, self)
+            pos = 1,1
+            self.schedule.add(a)
+            self.grid.place_agent(a, pos)
+
+        self.datacollector.collect(self)
+
+    def step(self):
+        '''Advance the model by one step.'''
+        self.schedule.step()
+        self.datacollector.collect(self)
+
+        # Determines if the model should continue running
+        if self.remaning_boxes == 0:
+            self.running = False
