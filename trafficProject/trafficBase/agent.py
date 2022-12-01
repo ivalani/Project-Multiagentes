1a494088249e0862b6496a5527db843954fe4775
Trh7101 <97247188+Trh7101@users.noreply.github.com>
2022-12-01 09:34:12 -0600

    peatones mamalones

--- trafficProject/trafficBase/agent.py
+++ trafficProject/trafficBase/agent.py
@@ -231,16 +231,48 @@ class Pedestrian(Agent):
             model: model reference
         """
         super().__init__(unique_id, model)
+        self.visited=[]
 
     def move(self):
+        if self.pos == None:
+            self.model.schedule.remove(self)
+            return
+
         posibleSteps = self.model.grid.get_neighbors(self.pos, moore=False, include_center=False, radius=1)
         isPedestrianViable = [agent.pos for agent in posibleSteps if isinstance(agent, PedestrianCrossing) or isinstance(agent, SideWalk) or isinstance(agent, Destination)  or isinstance(agent, Traffic_Light)]
         next_move = self.random.choice(isPedestrianViable)
-        whereIsDestination = self.model.grid.get_neighbors(next_move, moore=False, include_center=True, radius=0)
-        isDestination = [agent for agent in whereIsDestination if isinstance(agent, Destination)]
-        self.model.grid.move_agent(self, next_move)
-        if isDestination != []:
+        isPedestrianViable.remove(next_move)
+
+        while next_move in self.visited and isPedestrianViable != []:
+            next_move = self.random.choice(isPedestrianViable)
+            isPedestrianViable.remove(next_move)
+
+        self.visited.append(next_move)
+
+        whatIsFront = self.model.grid.get_neighbors(next_move, moore=False, include_center=True, radius=0)
+        notAPedestrian = [agent for agent in whatIsFront if not isinstance(agent, Pedestrian) or not isinstance(agent, Car)]
+
+        if isinstance(notAPedestrian[0], Traffic_Light):
+            if notAPedestrian[0].state == False:
+                self.model.grid.move_agent(self, next_move)
+                return
+            else:
+                self.visited.remove(next_move)
+                return
+        elif isinstance(notAPedestrian[0], PedestrianCrossing):
+            if notAPedestrian[0].state == None or notAPedestrian[0].state == "Pedestrian":
+                self.model.grid.move_agent(self, next_move)
+                return
+            else:
+                self.visited.remove(next_move)
+                return
+        elif isinstance(notAPedestrian, Destination):
             self.model.schedule.remove(self)
+            self.model.grid.move_agent(self, next_move)
+            return
+        else:
+            self.model.grid.move_agent(self, next_move)
+            return
 
     def step(self):
         self.move()
